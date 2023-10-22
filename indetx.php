20a176bcd8355ae5cca85a515b7315cf07bbf235
nekeng <148644592+nekeng@users.noreply.github.com>
2023-10-22 09:08:07 +0100

    Update indetx.php

--- indetx.php
+++ indetx.php
@@ -83,38 +83,3 @@
     </script>
 </body>
 </html>
-<!--  End of index.php file  -->
-Cilck save button then Cilck close button
-
-For the third file, click Create button again and select create file option type excel.php then Create button
-
-Double click excel.php file and paste the following code there
-
-<!--  excel.php file start here  -->
-
-<?php
-    header('Content-Type: application/xls');
-    header('Content-Disposition: attachment; filename=Readings.xls');
-?>
-<table>
-    <tr>
-        <th>Date (GMT or Iceland Time)</th>
-        <th>Readings</th>
-    </tr>
-    <?php
-        $selectDay = 0;
-        if (isset($_GET['selectDay1']) && $_GET['selectDay1']!="") {
-            $selectDay = strtotime($_GET['selectDay1']);
-        } else {
-            $selectDay = time()-43200;
-        }
-        $nextDay = $selectDay+86400;
-        $db = new PDO('sqlite:readingsDB.sqlite');
-        $query = "SELECT * FROM readings Where date  BETWEEN " .$selectDay." AND ".$nextDay." ORDER BY date DESC";
-        $result = $db->query($query);
-        foreach($result as $row) {
-            print "<tr><td>".date('d/m/Y H:i:s', $row['date'])."</td>";
-            print "<td>".$row['distance']."</td></tr>";
-        }
-    ?>
-</table>
